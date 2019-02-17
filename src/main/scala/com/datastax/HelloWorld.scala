85c6c15c9148e7524e08fd62a6a6a68dd44d1be7
Gilbert Lau <gmflau@gmail.com>
2019-02-16 16:45:45 -0800

    Added gitignore

---
+++ src/main/scala/com/datastax/HelloWorld.scala
@@ -0,0 +1,24 @@
+package com.datastax
+
+import com.datastax.spark.connector._
+import org.apache.spark._
+
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    // only setting app name, all other properties will be specified at runtime for flexibility
+
+    val conf = new SparkConf().setAppName("cassandra-example-hello")
+
+    val sc = new SparkContext(conf)
+
+    val hello = sc.cassandraTable[(String, String)]("test", "hello")
+
+    val first = hello.first
+
+    sc.stop
+
+    println(first)
+
+  }
+}
